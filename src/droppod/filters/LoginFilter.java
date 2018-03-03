c1b716b78069afad39b9e888dfdd66a88c1f8e9c
Lucas Melin <lucas.melin@gmail.com>
2018-03-03 16:51:29 -0500

    Added LoginFilter
    Uses Annotation library

---
+++ src/droppod/filters/LoginFilter.java
@@ -0,0 +1,32 @@
+package droppod.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/podcastServlet*")
+public class LoginFilter implements Filter{
+
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletRequest request = (HttpServletRequest) req;
+    HttpServletResponse response = (HttpServletResponse) resp;
+
+    HttpSession session = request.getSession(false);
+    if (session == null || session.getAttribute("name") == null) {
+        response.sendRedirect(request.getContextPath() + "/index.jsp");
+    } else {
+        chain.doFilter(request, response);
+    }
+    
+  }
+
+}
