2a58fd9d1dec568adfc32e82f8d348b80f2471e5
David <broo0207@algonquinlive.com>
2018-03-07 13:52:31 -0500

    Merged Login

--- droppod/src/droppod/login/LoginDroppod.java
+++ droppod/src/droppod/login/LoginDroppod.java
@@ -1,36 +1,45 @@
 package droppod.login;
 
+import droppod.bcrypt.BCrypt;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import droppod.executor.refreshFeedDroppod;
+
 public class LoginDroppod {
+	final static Logger logger = LoggerFactory.getLogger(LoginDroppod.class);
     public static boolean validate(String name, String pass) {        
         boolean status = false;
         Connection conn = null;
         PreparedStatement pst = null;
         ResultSet rs = null;
+        int passwordColumn = 3;
         
-        try {
-
+        try {       	
         	Context envContext = new InitialContext();
             Context initContext  = (Context)envContext.lookup("java:/comp/env");
             DataSource ds = (DataSource)initContext.lookup("jdbc/droppod");
-            //DataSource ds = (DataSource)envContext.lookup("java:/comp/env/jdbc/droppod");
             conn = ds.getConnection();
                          
             pst = conn
-            		.prepareStatement("select * from droppod.users where username=? and password=?");
+            		.prepareStatement("select * from droppod.users where username=?");
             pst.setString(1, name);
-            pst.setString(2, pass);
             rs = pst.executeQuery();
-            status = rs.next();
+            if (rs.next()) { //True if username exists in database otherwise false.
+            	if (BCrypt.checkpw(pass, rs.getString(passwordColumn))) {
+            		status = true;
+            	}
+            }
             
 
         } catch (Exception e) {
