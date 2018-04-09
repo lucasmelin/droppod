8967f4db809d883987fc4d604dcced82accfb85e
David <broo0207@algonquinlive.com>
2018-04-09 08:56:43 -0400

    Added some comments/documentation

--- src/droppod/servlets/FollowPodcastServlet.java
+++ src/droppod/servlets/FollowPodcastServlet.java
@@ -10,7 +10,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.servlet.RequestDispatcher;
@@ -20,7 +19,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
-
 import com.rometools.modules.itunes.FeedInformation;
 import com.rometools.rome.feed.module.Module;
 import com.rometools.rome.feed.synd.SyndEntry;
@@ -29,112 +27,116 @@ import com.rometools.rome.feed.synd.SyndImage;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
-
 import droppod.listen.ListenDroppod;
 import droppod.models.EpisodeModel;
 import droppod.models.PodcastModel;
 
 /**
- * Parses a given URL to determine if it corresponds to a podcast. If so,
- * podcast info is parsed and added to the database if it does not yet exist.
- * @author Lucas
+ * Parses a given URL to determine if it corresponds to a podcast. If so, podcast info is parsed and
+ * added to the database if it does not yet exist.
+ * 
+ * @author David
  *
  */
-public class FollowPodcastServlet extends HttpServlet{
+public class FollowPodcastServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 434245632236543L;
+  private static final long serialVersionUID = 434245632236543L;
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
 
-    	response.setContentType("text/html");  
-        PrintWriter out = response.getWriter();  
-        
-        HttpSession session = request.getSession(false);
-        
-        String uuid = session.getAttribute("uuid").toString();
-        String username = session.getAttribute("name").toString();
-             
-        int userID = 0;
-        
-        Connection con = null;
-        PreparedStatement pst = null;
-        PreparedStatement pst2 = null;
-        ResultSet rs = null;
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
 
+    HttpSession session = request.getSession(false);
+
+    // Retrieve session variables for queries
+    String uuid = session.getAttribute("uuid").toString();
+    String username = session.getAttribute("name").toString();
+
+    int userID = 0;
+
+    Connection con = null;
+    PreparedStatement pst = null;
+    PreparedStatement pst2 = null;
+    ResultSet rs = null;
+
+    try {
+
+      Context envContext = new InitialContext();
+      Context initContext = (Context) envContext.lookup("java:/comp/env");
+      DataSource ds = (DataSource) initContext.lookup("jdbc/droppod");
+      con = ds.getConnection();
+
+      /* Query 1 for getting userID of current user */
+      pst = con.prepareStatement("SELECT id FROM droppod.users WHERE username=?");
+      pst.setString(1, username);
+
+      rs = pst.executeQuery();
+      rs.next();
+      userID = rs.getInt(1);
+
+
+      /* Get podcast id by using the uuid */
+      pst = con.prepareStatement("SELECT id FROM droppod.podcasts WHERE uuid=?");
+      pst.setString(1, uuid);
+
+      rs = pst.executeQuery();
+      rs.next();
+      int podcastID = rs.getInt(1);
+
+
+      /* Query 2 for inserting userID and podcastID into user_follows table */
+      pst2 = con.prepareStatement(
+          "INSERT IGNORE INTO droppod.subscriptions (podcast_id, user_id) VALUES (?, ?)");
+      pst2.setInt(1, podcastID);
+      pst2.setInt(2, userID);
+
+      if (pst2.executeUpdate() == 1) {
+    	  
+    	// If query executes without error, update the session variable to include the new podcast(s)
+        String[] temp = (String[]) session.getAttribute("podcastIDs");
+        String[] newPod = new String[temp.length + 1];
+
+        for (int i = 0; i < temp.length; i++) {
+          newPod[i] = temp[i];
+        }
+
+        newPod[temp.length] = uuid;
+
+        for (int i = 0; i < newPod.length; i++) {
+          System.out.println(newPod[i]);
+        }
+
+        session.setAttribute("podcastIDs", newPod);
+      } else {
+    	// Otherwise, print error message
+        System.out.println("Execution Failed");
+      }
+
+    } catch (Exception e) {
+      System.out.println("Exception: " + e.getMessage());
+    } finally {
+      if (con != null) {
         try {
-        	
-        	Context envContext = new InitialContext();
-            Context initContext  = (Context)envContext.lookup("java:/comp/env");
-            DataSource ds = (DataSource)initContext.lookup("jdbc/droppod");          
-            con = ds.getConnection();
-                        
-            /* Query 1 for getting userID of current user */
-            pst = con.prepareStatement("SELECT id FROM droppod.users WHERE username=?");
-            pst.setString(1, username);
-            
-            rs = pst.executeQuery();
-            rs.next();
-            userID = rs.getInt(1);
-            
-          
-           /* Get podcast id by using the uuid */
-            pst = con.prepareStatement("SELECT id FROM droppod.podcasts WHERE uuid=?");
-            pst.setString(1, uuid);
-            
-            rs = pst.executeQuery();
-            rs.next();
-            int podcastID = rs.getInt(1);
-            
-            
-            /* Query 2 for inserting userID and podcastID into user_follows table */
-            pst2 = con.prepareStatement("INSERT IGNORE INTO droppod.user_follows (podcast_id, user_id) VALUES (?, ?)");  
-            pst2.setInt(1, podcastID);
-            pst2.setInt(2, userID);
-        	
-            if (pst2.executeUpdate() == 1) {
-            	System.out.println("SUCCESS");
-            	
-            	String[] temp = (String[]) session.getAttribute("podcastIDs");
-            	
-            	String[] newPod = new String[temp.length];
-            	
-            	for (int i = 0; i < temp.length; i++) {
-            		newPod[i] = temp[i];
-            	}
-            	
-            	newPod[temp.length - 1] = uuid;
-            	
-            	for (int i = 0; i < newPod.length; i++) {
-            		System.out.println(newPod[i]);
-            	}
-            	            	
-            	session.setAttribute("podcastIDs", newPod);
-            } else {
-            	System.out.println("YOU SUCK");
-            }
-            
-        } catch (Exception e) {
-        	System.out.println("Exception: " + e.getMessage());
-        } finally {
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (pst != null) {
-                try {
-                    pst.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
+          con.close();
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+      if (pst != null) {
+        try {
+          pst.close();
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
 
-            RequestDispatcher rd=request.getRequestDispatcher("following.jsp");  
-            rd.include(request,response); 
-            
-            out.close();  
-        } 
+      // Once the podcast has been followed, redirect user to the Follow page
+      RequestDispatcher rd = request.getRequestDispatcher("following.jsp");
+      rd.include(request, response);
+
+      out.close();
     }
+  }
 }
